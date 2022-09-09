530df2dcada292fb72cf3f10b0c88b532bdf953f
kasamatsu <kasamatsu@kasa.com>
2022-09-09 15:24:38 +0900

    CoreObject_AudioQueryまで

--- VoicevoxCoreNet.Tests/VoicevoxCoreNet_IsEvenShould.cs
+++ VoicevoxCoreNet.Tests/VoicevoxCoreNet_IsEvenShould.cs
@@ -9,10 +9,76 @@ public class VoicevoxCoreNet_IsEvenShould
     }
 
     [Fact]
-    public void CoreObject_LoadModel()
+    public void CoreObject_IsGpuMode()
+    {
+        // Given
+        Core core = GetDefaultCoreObject();
+        // When
+        bool isGpuMode = core.IsGpuMode;
+        // Then
+        Assert.False(isGpuMode);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void CoreObject_LoadModel(uint speakerId)
     {
         Core core = GetDefaultCoreObject();
-        core.LoadModel(0);
+        core.LoadModel(speakerId);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void CoreObject_IsModelLoaded(uint speakerId)
+    {
+        // Given
+        Core core = GetDefaultCoreObject();
+        // When
+        bool isModelLoaded = core.IsModelLoaded(speakerId);
+        // Then
+        Assert.False(isModelLoaded);
+    }
+
+    [Fact]
+    public void CoreObject_GetMetasJson()
+    {
+        // Given
+        Core core = GetDefaultCoreObject();
+        // When
+        string json = core.GetMetasJson();
+        // Then
+        Assert.NotEmpty(json);
+    }
+
+    [Fact]
+    public void CoreObjct_GetSupportedDevicesJson()
+    {
+        // Given
+        Core core = GetDefaultCoreObject();
+        // When
+        string json = core.GetSupportedDevicesJson();
+        // Then
+        Assert.NotEmpty(json);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void CoreObject_AudioQuery(uint speakerId)
+    {
+        // Given
+        Core core = GetDefaultCoreObject();
+        core.LoadModel(speakerId);
+        AudioQueryOptions options = new AudioQueryOptions()
+        {
+            kana = false
+        };
+        // When
+        string json = core.AudioQuery("あいうえお", speakerId, options);
+        // Then
+        Assert.NotEmpty(json);
     }
 
     /// <summary>
@@ -22,6 +88,7 @@ public class VoicevoxCoreNet_IsEvenShould
     public Core GetDefaultCoreObject()
     {
         InitializeOptions options = new InitializeOptions(GetOpenJtalkDictDir());
+        options.LoadAllModels = false;
         return new Core(options);
     }
 
